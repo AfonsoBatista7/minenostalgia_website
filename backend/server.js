9e07eb5069017e9c35556b1e6a77517213914d7d
AfonsoBatista07 <afonsobatista13@gmail.com>
2024-11-13 17:06:08 +0000

    [Update] - added cors and express to server

--- backend/server.js
+++ backend/server.js
@@ -1,7 +1,15 @@
 require('dotenv').config();  // Load environment variables from .env
 
 const mongoose = require('mongoose');
+const express = require('express');
+const cors = require('cors');
 
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+// Middleware
+app.use(cors());
+app.use(express.json());
 
 // MongoDB Connection
 mongoose.set('strictQuery', false);
@@ -13,3 +21,6 @@ mongoose.connect(process.env.MONGODB_TOKEN)
     console.log(error);
 });
 
+app.listen(PORT, () => {
+    console.log(`Server is running on http://localhost:${PORT}`);
+});
